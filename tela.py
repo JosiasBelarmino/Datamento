b39502b4043d22df7db32fb51880ee19ba9dcf0c
JosiasBelarmino <josiasser@gmail.com>
2023-10-15 10:21:50 -0300

    atualização do projeto
    iniciei a adição dos campos de input

--- tela.py
+++ tela.py
@@ -6,7 +6,7 @@ class application():
         self.root = root
         self.tela()
         self.frames_da_tela()
-        self.criando_botoes()
+        self.widgets_frame_1()
         root.mainloop()
     def tela(self):
         self.root.title("Datamento")
@@ -24,26 +24,54 @@ class application():
                              highlightbackground = "#FFA78B", highlightthickness = 3)
         self.frame_2.place(relx = 0.01 , rely = 0.5, relwidth = 0.98, relheight = 0.46)
     
-    def criando_botoes(self):
+    def widgets_frame_1(self):
         #criação do botão de limpar
         self.bt_limpar = Button(self.frame_1, text = "Limpar")
         self.bt_limpar.place(relx = 0.2 , rely = 0.1, relwidth= 0.1, relheight= 0.15 )
 
          #criação do botão de Buscar
         self.bt_limpar = Button(self.frame_1, text = "Buscar")
-        self.bt_limpar.place(relx = 0.31 , rely = 0.1, relwidth= 0.1, relheight= 0.15 )
+        self.bt_limpar.place(relx = 0.31 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
 
         #criação do botão de Novo
         self.bt_limpar = Button(self.frame_1, text = "Novo")
-        self.bt_limpar.place(relx = 0.42 , rely = 0.1, relwidth= 0.1, relheight= 0.15 )
+        self.bt_limpar.place(relx = 0.42 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
 
         #criação do botão de Alterar
         self.bt_limpar = Button(self.frame_1, text = "Alterar")
-        self.bt_limpar.place(relx = 0.53 , rely = 0.1, relwidth= 0.1, relheight= 0.15 )
+        self.bt_limpar.place(relx = 0.53 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
 
         #criação do botão de Apagar
         self.bt_limpar = Button(self.frame_1, text = "Apagar")
-        self.bt_limpar.place(relx = 0.64 , rely = 0.1, relwidth= 0.1, relheight= 0.15 )
+        self.bt_limpar.place(relx = 0.64 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
+
+        #Criação da lapel e entrada do Codigo
+        self.lb_codigo = Label(self.frame_1, text = "Código")
+        self.lb_codigo.place(relx = 0.076 , rely = 0.05)
+
+        self.codigo_entry = Entry(self.frame_1)
+        self.codigo_entry.place(relx = 0.02 , rely = 0.15, relheight =0.1 , relwidth = 0.15)
+
+        #Criação da lapel e entrada do Nome
+        self.lb_nome = Label(self.frame_1, text = "Nome")
+        self.lb_nome.place(relx = 0.076 , rely = 0.35)
+
+        self.nome_entry = Entry(self.frame_1)
+        self.nome_entry.place(relx = 0.02 , rely = 0.45, relheight =0.1 , relwidth = 0.4)
+
+        #Criação da lapel e entrada do Telefone
+        self.lb_telefone = Label(self.frame_1, text = "Telefone")
+        self.lb_telefone.place(relx = 0.076 , rely = 0.35)
+
+        self.telefone_entry = Entry(self.frame_1)
+        self.telefone_entry.place(relx = 0.02 , rely = 0.45, relheight =0.1 , relwidth = 0.4)
+
+        #Criação da lapel e entrada do Cidade
+        self.lb_cidade = Label(self.frame_1, text = "Cidade")
+        self.lb_cidade.place(relx = 0.076 , rely = 0.35)
+
+        self.cidade_entry = Entry(self.frame_1)
+        self.cidade_entry.place(relx = 0.02 , rely = 0.45, relheight =0.1 , relwidth = 0.4)
 
 
 

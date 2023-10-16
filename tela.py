3b7cde0e56521d6383f684bf319bf3ee640ef7c6
JosiasBelarmino <josiasser@gmail.com>
2023-10-16 18:29:21 -0300

    Update tela.py

--- tela.py
+++ tela.py
@@ -8,7 +8,7 @@ class application():
         self.tela()
         self.frames_da_tela()
         self.widgets_frame_1()
-        self.lista_frame_2
+        self.lista_frame_2()
         root.mainloop()
     def tela(self):
         self.root.title("Datamento")
@@ -33,24 +33,24 @@ class application():
         self.bt_limpar.place(relx = 0.2 , rely = 0.1, relwidth= 0.1, relheight= 0.15 )
 
          #criação do botão de Buscar
-        self.bt_limpar = Button(self.frame_1, text = "Buscar",bd = 1, bg = "#547EA0", fg = "white",
+        self.bt_buscar = Button(self.frame_1, text = "Buscar",bd = 1, bg = "#547EA0", fg = "white",
                                 font = ("verdana", 9, "bold"))
-        self.bt_limpar.place(relx = 0.31 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
+        self.bt_buscar.place(relx = 0.31 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
 
         #criação do botão de Novo
-        self.bt_limpar = Button(self.frame_1, text = "Novo",bd = 1, bg = "#547EA0", fg = "white",
+        self.bt_novo = Button(self.frame_1, text = "Novo",bd = 1, bg = "#547EA0", fg = "white",
                                 font = ("verdana", 9, "bold"))
-        self.bt_limpar.place(relx = 0.42 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
+        self.bt_novo.place(relx = 0.42 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
 
         #criação do botão de Alterar
-        self.bt_limpar = Button(self.frame_1, text = "Alterar",bd = 1, bg = "#547EA0", fg = "white",
+        self.bt_alterar = Button(self.frame_1, text = "Alterar",bd = 1, bg = "#547EA0", fg = "white",
                                 font = ("verdana", 9, "bold"))
-        self.bt_limpar.place(relx = 0.53 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
+        self.bt_alterar.place(relx = 0.53 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
 
         #criação do botão de Apagar
-        self.bt_limpar = Button(self.frame_1, text = "Apagar",bd = 1, bg = "#547EA0", fg = "white",
+        self.bt_apagar = Button(self.frame_1, text = "Apagar",bd = 1, bg = "#547EA0", fg = "white",
                                 font = ("verdana", 9, "bold"))
-        self.bt_limpar.place(relx = 0.64 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
+        self.bt_apagar.place(relx = 0.64 , rely = 0.1, relwidth = 0.1, relheight = 0.15 )
 
         #Criação da lapel e entrada do Codigo
         self.lb_codigo = Label(self.frame_1, text = "Código", fg = "#4E8397", font = ("verdana", 9, "bold"))
@@ -82,7 +82,7 @@ class application():
 
     def lista_frame_2(self):
         #Definindo quantidade de colunas
-        self.listaCli = ttk.Treeviwe(self.frame_2, height = 3, column = ("col1", "col2", "col3", "col4"))
+        self.listaCli = ttk.Treeview(self.frame_2, height = 3, column = ("col1", "col2", "col3", "col4"))
        
         #Nomeando Colunas definidas
         self.listaCli.heading("#0", text = "")
@@ -92,14 +92,22 @@ class application():
         self.listaCli.heading("#4", text = "Cidade")
 
         #Tamanho das colunas, lembrando que a coluna 0 tem que ser declarada mas não aparece
-        self.listaCli.column("$0", width = 1)
-        self.listaCli.column("$1", width = 50)
-        self.listaCli.column("$2", width = 200)
-        self.listaCli.column("$3", width = 125)
-        self.listaCli.column("$4", width = 125)
+        self.listaCli.column("#0", width = 0)
+        self.listaCli.column("#2", width = 200)
+        self.listaCli.column("#3", width = 125)
+        self.listaCli.column("#4", width = 125)
 
         #Declaração posições da lista
         self.listaCli.place(relx = 0.01, rely = 0.01, relwidth = 0.95, relheight = 0.85)
+
+        #barra de rolagem
+        #Posição
+        self.scroolLista = Scrollbar(self.frame_2, orient = "vertical")
+        #A quem a barra percente (esta vinculada)
+        self.listaCli.configure(yscroll = self.scroolLista.set)
+        #Onde ela fica
+        self.scroolLista.place(relx = 0.96, rely = 0.1, relwidth = 0.04, relheight = 0.85)
+
 
 
 
